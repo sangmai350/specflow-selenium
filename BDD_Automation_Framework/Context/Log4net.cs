f193dd543b22a1b21667b3f1a6a66f253be2333e
sang mai <sangmai350@gmail.com>
2020-03-26 11:50:12 +0700

    Update report

--- BDD_Automation_Framework/Context/Log4net.cs
+++ BDD_Automation_Framework/Context/Log4net.cs
@@ -1,11 +1,21 @@
 ﻿using log4net;
 using System;
+using log4net.Layout;
 
 namespace Logging
 {
-    public class Logger
+    public class Logger : PatternLayout
     {
         private readonly ILog log = null;
+
+        public override string Header
+        {
+            get
+            {
+                var dateString = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                return string.Format("[START:  {0} ]\r\n", dateString);
+            }
+        }
 
         public Logger()
         {
